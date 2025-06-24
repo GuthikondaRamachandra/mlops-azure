f3fec389d57af01f2283993fe4df8902ca42946a
GuthikondaRamachandra <71042730+GuthikondaRamachandra@users.noreply.github.com>
2025-06-24 22:27:57 +0100

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,3 +5,7 @@ output "resource_groups" {
 output "key_vaults" {
   value = { for env in var.environments : env => azurerm_key_vault.mlops_env_kv[env].name }
 }
+
+output "databricks_urls" {
+  value = { for env in var.environments : env => azurerm_databricks_workspace.mlops_env_ws[env].workspace_url }
+}
