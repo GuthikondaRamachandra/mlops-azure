8f33abf0bb0592dccfd2e3c5bfa92fc6acbe05ba
GuthikondaRamachandra <71042730+GuthikondaRamachandra@users.noreply.github.com>
2025-06-24 21:49:59 +0100

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,16 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.mlops_rg.name
+}
+
+output "key_vault_dev" {
+  value = azurerm_key_vault.dev.name
+}
+
+output "key_vault_qa" {
+  value = azurerm_key_vault.qa.name
+}
+
+output "key_vault_prod" {
+  value = azurerm_key_vault.prod.name
+}
 
