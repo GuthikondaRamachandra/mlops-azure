4766155cc5dadbc15ae398db9b29eec1b9f86aa7
GuthikondaRamachandra <71042730+GuthikondaRamachandra@users.noreply.github.com>
2025-06-24 23:09:23 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -28,11 +28,3 @@ resource "azurerm_key_vault" "mlops_env_kv" {
   purge_protection_enabled    = false
 }
 
-resource "azurerm_databricks_workspace" "mlops_env_ws" {
-  for_each = toset(var.environments)
-
-  name                = "databricks-${var.project_name}-${each.key}"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.mlops_env[each.key].name
-  sku                 = "standard"
-}
