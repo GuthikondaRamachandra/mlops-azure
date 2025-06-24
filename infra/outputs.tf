394c14f3a467892b235b15ace8ae8b80f1b8da1a
GuthikondaRamachandra <71042730+GuthikondaRamachandra@users.noreply.github.com>
2025-06-24 22:09:52 +0100

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,16 +1,7 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.mlops_rg.name
+output "resource_groups" {
+  value = { for env in var.environments : env => azurerm_resource_group.mlops_env[env].name }
 }
 
-output "key_vault_dev" {
-  value = azurerm_key_vault.dev.name
+output "key_vaults" {
+  value = { for env in var.environments : env => azurerm_key_vault.mlops_env_kv[env].name }
 }
-
-output "key_vault_qa" {
-  value = azurerm_key_vault.qa.name
-}
-
-output "key_vault_prod" {
-  value = azurerm_key_vault.prod.name
-}
-
