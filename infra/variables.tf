101c94f4c5e3748f1610fec84cbe2f8d36f307e8
GuthikondaRamachandra <71042730+GuthikondaRamachandra@users.noreply.github.com>
2025-06-24 22:07:18 +0100

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,22 @@
-variable "tenant_id" {
+variable "project_name" {
+  description = "Base name for resources"
   type        = string
-  description = "Azure Tenant ID"
+  default     = "mlops"
 }
 
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "environments" {
+  description = "Deployment environments"
+  type        = list(string)
+  default     = ["dev", "qa", "prod"]
+}
