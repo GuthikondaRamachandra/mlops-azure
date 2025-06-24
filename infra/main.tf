a981897a3663fe098f79c6648585b709d7374c08
GuthikondaRamachandra <71042730+GuthikondaRamachandra@users.noreply.github.com>
2025-06-24 23:44:04 +0100

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -6,10 +6,28 @@ terraform {
     }
   }
 }
+
 provider "azurerm" {
   features {}
 }
 
+resource "azurerm_resource_group" "mlops_env" {
+  for_each = toset(var.environments)
+
+  name     = "rg-${var.project_name}-${each.key}"
+  location = var.location
+}
+
+resource "azurerm_key_vault" "mlops_env_kv" {
+  for_each = toset(var.environments)
+
+  name                        = "kv-${var.project_name}-${each.key}"
+  location                    = var.location
+  resource_group_name         = azurerm_resource_group.mlops_env[each.key].name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+  purge_protection_enabled    = false
+}
 
 resource "azurerm_databricks_workspace" "mlops_env_ws" {
   for_each = toset(var.environments)
